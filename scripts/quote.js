594f8c5d26e11e773bbefda741e1162e43a0bee5
Rasul Semenov <semenovrasul7@gmail.com>
2025-11-09 21:15:58 +0500

    Final

--- scripts/quote.js
+++ scripts/quote.js
@@ -2,7 +2,7 @@ async function getQuote() {
   try {
     const response = await fetch("https://api.allorigins.win/raw?url=https://api.adviceslip.com/advice");
     const data = await response.json();
-    document.getElementById("quote").textContent = `"${data.slip.advice}" — AdviceBot 🤖`;
+    document.getElementById("quote").textContent = `"${data.slip.advice}"`;
   } catch (error) {
     console.error("Error fetching advice:", error);
     document.getElementById("quote").textContent = "⚠️ Failed to load advice!";
